6c22c9d11ba45eb077cf7aa033f39c08e2344fb1
Jhonatan Rian <64628254+JhonatanRian@users.noreply.github.com>
2022-11-02 00:20:59 -0300

    Create Exercicio.java

---
+++ Exercicio.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class Exercicio {
+
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        int A, B, C; //Escreva um programa que leia três valores para os lados de um triângulo
+
+      	System.out.print("Verificador de Triângulo\n\n");
+
+
+        System.out.print("Digite o lado A=");
+        A = entrada.nextInt(); //Escreva um programa que leia três valores para os lados de um triângulo
+		
+        System.out.print("Digite o lado B=");
+        B = entrada.nextInt(); //Escreva um programa que leia três valores para os lados de um triângulo
+		
+		System.out.print("Digite o lado C=");
+        C = entrada.nextInt(); //Escreva um programa que leia três valores para os lados de um triângulo
+
+
+		if( A < B + C && B < A + C && C < A + B ){ //Verificar se cada lado é menor que a soma dos outros dois lados.
+			if(A==B && B == C){ //saber de A==B e se B==C, sendo verdade o triângulo é eqüilátero
+				System.out.print("O triângulo é Eqüilátero.");
+			}else if(A==B || A==C || B==C){ //Se não, verificar de A==B ou se A==C ou se B==C, sendo verdade o triângulo é isósceles
+				System.out.print("O triângulo é Isósceles.");
+			}else{ //e caso contrário, o triângulo será escaleno
+				System.out.print("O triângulo é Escaleno.");
+			}
+		}else{ //Caso os lados fornecidos não caracterizarem um triângulo, avisar a ocorrência.
+			System.out.print("Os lados fornecidos não caracterizam um triângulo");
+		}
+
+    }
+
+}
